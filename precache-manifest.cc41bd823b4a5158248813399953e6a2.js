9879c733c236f36e03485936bd214e82327d2bb9
SilentDubich <kirill.dubov.2012@mail.ru>
2022-05-04 21:29:23 +0300

    Updates

---
+++ precache-manifest.cc41bd823b4a5158248813399953e6a2.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf907dd6d080dca6f199f6f948c87117",
+    "url": "/test-sociaro/index.html"
+  },
+  {
+    "revision": "3ae424b575b6d517856f",
+    "url": "/test-sociaro/static/css/main.76adde1b.chunk.css"
+  },
+  {
+    "revision": "34a46f697105e66ccb08",
+    "url": "/test-sociaro/static/js/2.1cf43f46.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-sociaro/static/js/2.1cf43f46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ae424b575b6d517856f",
+    "url": "/test-sociaro/static/js/main.51353549.chunk.js"
+  },
+  {
+    "revision": "0a2b9be222f4524d6d79",
+    "url": "/test-sociaro/static/js/runtime-main.9ffa1dd6.js"
+  },
+  {
+    "revision": "4b7d2a000a8e5960143e77618d1e77d7",
+    "url": "/test-sociaro/static/media/Spinner-1.4s-3a356a.4b7d2a00.svg"
+  },
+  {
+    "revision": "34951eba3a0055c430f7fba4e1aaa421",
+    "url": "/test-sociaro/static/media/day.34951eba.svg"
+  },
+  {
+    "revision": "98b6e7bcf89924f15abe5b2a11243da5",
+    "url": "/test-sociaro/static/media/humidity.98b6e7bc.svg"
+  },
+  {
+    "revision": "66e90e8d03b8cca6045d4a7e7ad01c1f",
+    "url": "/test-sociaro/static/media/night.66e90e8d.svg"
+  },
+  {
+    "revision": "e8bba4f97f32756224582ca6df1174bb",
+    "url": "/test-sociaro/static/media/pressure.e8bba4f9.svg"
+  },
+  {
+    "revision": "84e55b9655007e42882091085abeba70",
+    "url": "/test-sociaro/static/media/sand-clock.84e55b96.svg"
+  },
+  {
+    "revision": "35e1634c3dbe533bf53163c5eaafbe29",
+    "url": "/test-sociaro/static/media/sunrise.35e1634c.svg"
+  },
+  {
+    "revision": "6b4fc41a4d91f5c70d715626b6c6cd47",
+    "url": "/test-sociaro/static/media/sunset.6b4fc41a.svg"
+  },
+  {
+    "revision": "8aa75e6ad56e68432f9538b226bbe894",
+    "url": "/test-sociaro/static/media/tag.8aa75e6a.svg"
+  },
+  {
+    "revision": "96892bc1d519459448dc43dcc20c57af",
+    "url": "/test-sociaro/static/media/wind.96892bc1.svg"
+  }
+]);
